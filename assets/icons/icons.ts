2c319d491e3a254bc3d8b7196c8a0fca782c97dd
endertopaloglu <endertopaloglu@gmail.com>
2024-01-02 20:53:09 +0300

    svg imajı göstermek için çıldırıldı ve paket sağlıklı yüklendi.

---
+++ assets/icons/icons.ts
@@ -0,0 +1,4 @@
+import bell from './bell.svg';
+import megaphone from './megaphone.svg';
+
+export { bell, megaphone };
--- screens/Home/Header.tsx
+++ screens/Home/Header.tsx
@@ -1,7 +1,9 @@
+import React from 'react';
 import { getColors } from '../../src/styles/colors';
 import { View, Text } from 'react-native';
 import { distances, systemSizes } from '../../src/styles/sizes';
 import { typography } from '../../src/styles/typography';
+import { bell } from '../../assets/icons/icons'
 
 const Header = () => {
     const colors = getColors();
@@ -11,6 +13,7 @@ const Header = () => {
     <View style={{backgroundColor:colors.Bg_white}}>
         <View style={{ backgroundColor:colors.Main_Adaptive_light, paddingTop:systemSizes.ios_safeTop, paddingHorizontal:distances._20, paddingVertical:distances._16 }}>
             <Text style={typo.h3_Bold}>Paribu</Text>
+            <bell width={30} height={30} />
         </View>
     </View>
   );
